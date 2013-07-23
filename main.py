381e982eb04c88447998b046e7fcb372be31273a
Rodrigo Deodoro <roddds@gmail.com>
2013-07-23 13:38:30 -0300

    new overall structure

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "Main page"
+    return render_template('main.html')
 
 if __name__ == '__main__':
     app.run()
