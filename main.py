20c7bd211435fc2d8229f0be2d2b7210cde09328
Ubuntu <ubuntu@ip-172-31-22-83.sa-east-1.compute.internal>
2013-08-27 17:39:58 +0000

    added 404 error page

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def home():
     return render_template('main.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000)
