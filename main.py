db69e2cce1e8ff8197e8794e866f54502bf74cee
Rodrigo Deodoro <rodrigo.deodoro@concretesolutions.com.br>
2013-09-03 14:25:28 -0300

    padronização das aspas

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('main.html')
+    return render_template("main.html")
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html')
+    return render_template("404.html")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
