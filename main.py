93605abcc2d05805219403e124fbaa34584121e8
Rodrigo Deodoro <roddds@gmail.com>
2016-03-06 16:44:33 -0300

    shebang, debug

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -12,4 +13,4 @@ def page_not_found(e):
     return render_template("404.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8000, debug=True)
